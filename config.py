25496e095b768aad370bc28818d66cac65a972d1
Enthusiastic <60442667+harshita897@users.noreply.github.com>
2020-07-08 21:11:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-# twitterbot/bots/config.py
 import tweepy
 import logging
 import os
@@ -7,13 +6,13 @@ logger = logging.getLogger()
 
 
 def create_api():
-    consumer_key = os.getenv('eY9qQrIZaDuzkmg49NoXVThFX')
-    consumer_secret = os.getenv('hKO2UOmHJ68d00UF3YLU84bYV5f82aKPk8krpfxex7lp7jXJFo')
-    access_token = os.getenv('1256632516397731841-nhHPnZD8nJHxyRwZrch3Mk0WCDMg5s')
-    access_token_secret = os.getenv('7vzEMP7eYTZVaznhTTnfPcmydk5DXO0Gebexbz3UD0jKW')
+    consumer_key = os.getenv('consumer_key')
+    consumer_secret = os.getenv('consumer_secret')
+    access_token = os.getenv('access_token')
+    access_token_secret = os.getenv('access_token_secret')
 
-    auth = tweepy.OAuthHandler('eY9qQrIZaDuzkmg49NoXVThFX','hKO2UOmHJ68d00UF3YLU84bYV5f82aKPk8krpfxex7lp7jXJFo')
-    auth.set_access_token('1256632516397731841-nhHPnZD8nJHxyRwZrch3Mk0WCDMg5s','7vzEMP7eYTZVaznhTTnfPcmydk5DXO0Gebexbz3UD0jKW')
+    auth = tweepy.OAuthHandler('consumer_key','consumer_secret')
+    auth.set_access_token('access_token','access_token_secret')
     api = tweepy.API(auth, wait_on_rate_limit=True,
                      wait_on_rate_limit_notify=True)
     try:
